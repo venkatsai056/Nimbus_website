0c1fdf570c86ce80f802b8526749c24673a14581
venkatsai056 <90790292+venkatsai056@users.noreply.github.com>
2025-07-09 12:24:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ st.markdown("""
 
 # Company Name and Logo
 st.title("Welcome to Nimbus Insights")
-st.image(https://www.nimbusinsights.cloud/assets/logo-JHWB-tWI.png)
+st.image("https://www.nimbusinsights.cloud/assets/logo-JHWB-tWI.png")
 
 # Navigation
 st.sidebar.title("Navigation")
